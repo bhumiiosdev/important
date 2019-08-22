35d96dcd4c68080988a76ec10dea29a34dbddc71
bhumiiosdev <40639827+bhumiiosdev@users.noreply.github.com>
2019-08-22 11:28:44 +0530

    ImageOntext

---
+++ AddTextOnImage.swift
@@ -0,0 +1,42 @@
+//
+//  ViewController.swift
+//  Text
+//
+//  Created by PC on 26/10/18.
+//  Copyright © 2018 Opula Software. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    @IBOutlet weak var imageView: UIImageView!
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        self.imageView.image = self.textToImage(drawText: "testing", inImage: UIImage(named: "bg")!, atPoint: CGPoint.init(x: 50, y: 0))
+    }
+
+    func textToImage(drawText text: String, inImage image: UIImage, atPoint point: CGPoint) -> UIImage {
+        let textColor = UIColor.white
+        let textFont = UIFont(name: "Helvetica Bold", size: 25)!
+        
+        let scale = UIScreen.main.scale
+        UIGraphicsBeginImageContextWithOptions(image.size, false, scale)
+        
+        let textFontAttributes = [
+            NSAttributedStringKey.font: textFont,
+            NSAttributedStringKey.foregroundColor: textColor,
+            ] as [NSAttributedStringKey : Any]
+        image.draw(in: CGRect(origin: CGPoint.zero, size: image.size))
+        
+        let rect = CGRect(origin: point, size: image.size)
+        text.draw(in: rect, withAttributes: textFontAttributes)
+        
+        let newImage = UIGraphicsGetImageFromCurrentImageContext()
+        UIGraphicsEndImageContext()
+        
+        return newImage!
+    }
+
+}
+
